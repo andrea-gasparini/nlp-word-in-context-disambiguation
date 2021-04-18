08e22b19481801ef2c3175e3bf362dbd60277d32
Andrea Gasparini <gasptube@gmail.com>
2021-04-18 19:06:22 +0200

    Refactor sample2vector function

--- hw1/stud/utils.py
+++ hw1/stud/utils.py
@@ -1,16 +1,15 @@
 import torch
 import re
 
-from typing import Optional, Dict
+from typing import Optional, List
 from stud.word_embeddings import WordEmbeddings
 
 
-def sample2vector(word_embeddings: WordEmbeddings, sample: Dict) -> Optional[torch.Tensor]:
-    sentence1 = remove_special_characters(sample['sentence1']).lower()
-    sentence2 = remove_special_characters(sample['sentence2']).lower()
-    sentences_concat = sentence1 + " | " + sentence2
+def sample2vector(word_vectors: WordEmbeddings, sample: dict, separator: str = '|', target_weight: int = 10) -> Optional[torch.Tensor]:
+    sentence1 = sentence2embeddings(word_vectors, sample['sentence1'], int(sample['start1']), target_weight)
+    sentence2 = sentence2embeddings(word_vectors, sample['sentence2'], int(sample['start2']), target_weight)
 
-    sentences_word_vector = [word_embeddings[word] for word in sentences_concat.split(' ')]
+    sentences_word_vector = sentence1 + [word_vectors[separator]] + sentence2
 
     if len(sentences_word_vector) == 0:
         return None
@@ -20,11 +19,28 @@ def sample2vector(word_embeddings: WordEmbeddings, sample: Dict) -> Optional[tor
     return torch.mean(sentences_word_vector, dim=0)
 
 
-def remove_special_characters(string: str) -> str:
-    string = re.sub(r'[\-—]', ' ', string)
-    string = re.sub(r'[.,:;|!?@#$"“”()\[\]&\\<>/0-9]', '', string)
+def sentence2embeddings(word_vectors: WordEmbeddings, sentence: str, target_start: int, target_weight: int) -> List[torch.Tensor]:
+    sentence = substitute_spacing_characters(sentence).lower()
+    target_word_index = sentence[:target_start].count(' ')
 
-    return string
+    sentence_word_vector = list()
+
+    for i, word in enumerate(sentence.split(' ')):
+        word = remove_special_characters(word)
+        word_vector = word_vectors[word]
+        if i == target_word_index:
+            word_vector * target_weight
+        sentence_word_vector.append(word_vector)
+
+    return sentence_word_vector
+
+
+def substitute_spacing_characters(string: str) -> str:
+    return re.sub(r'[\-—_/]', ' ', string)
+
+
+def remove_special_characters(string: str) -> str:
+    return re.sub(r'[.,:;|!?@#$"“”\'’()\[\]&\\<>0-9]', '', string)
 
 
 def str_to_bool(s: str) -> bool:
