09efd5602804948a68731855176a8a82b3df7941
Andrea Gasparini <gasptube@gmail.com>
2021-04-25 18:18:20 +0200

    Added English stop words and load function

--- hw1/stud/utils.py
+++ hw1/stud/utils.py
@@ -43,3 +43,12 @@ def remove_special_characters(string: str) -> str:
 
 def str_to_bool(s: str) -> bool:
     return s == 'True'
+
+
+def load_stop_words(path: str) -> Set[str]:
+    stop_words = set()
+    with open(path) as f:
+        for line in f:
+            stop_words.add(line.strip())
+
+    return stop_words
