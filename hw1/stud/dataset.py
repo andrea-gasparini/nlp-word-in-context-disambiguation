46346f740ea4212768a3cc3fcbcbc0f8bc23137e
Andrea Gasparini <andrea.gasparini11@gmail.com>
2021-05-02 12:51:14 +0200

    Update final word-level model

--- hw1/stud/dataset.py
+++ hw1/stud/dataset.py
@@ -4,37 +4,42 @@ import stud.utils as utils
 
 from torch import Tensor
 from torch.utils.data import Dataset
-from typing import Callable, List, Tuple, Dict, Optional
+from typing import List, Tuple, Dict, Optional, Set
 from stud.word_embeddings import WordEmbeddings
 
 
 class WiCDisambiguationDataset(Dataset):
+    target_word_weight = 10
 
-    def __init__(self, samples: List[Dict], word_embeddings: WordEmbeddings,
-                 sample2vector: Callable[[WordEmbeddings, Dict], Optional[Tensor]]) -> None:
+    def __init__(self, samples: List[Dict], word_embeddings: WordEmbeddings, stop_words: Optional[Set[str]] = None) -> None:
         self.samples = samples
         self.word_embeddings = word_embeddings
-        self.sample2vector = sample2vector
+        self.stop_words = stop_words
 
         self.encoded_samples, self.encoded_labels = self.encode_samples(self.samples)
 
     @staticmethod
-    def from_file(path: str, word_embeddings: WordEmbeddings,
-                  sample2vector: Callable[[WordEmbeddings, Dict], Optional[Tensor]]) -> 'WiCDisambiguationDataset':
+    def from_file(path: str, word_embeddings: WordEmbeddings, stop_words: Optional[Set[str]] = None) -> 'WiCDisambiguationDataset':
         samples = list()
 
         with jsonlines.open(path) as f:
             for sample in f:
                 samples.append(sample)
 
-        return WiCDisambiguationDataset(samples, word_embeddings, sample2vector)
+        return WiCDisambiguationDataset(samples, word_embeddings, stop_words)
 
     def encode_samples(self, samples: List[Dict]) -> Tuple[List[Tensor], List[Optional[Tensor]]]:
         encoded_samples = list()
         encoded_labels = list()
 
         for sample in samples:
-            encoded_samples.append(self.sample2vector(self.word_embeddings, sample))
+            sentence1_vector = self.__sentence_to_vector(sample['sentence1'], int(sample['start1']))
+            sentence2_vector = self.__sentence_to_vector(sample['sentence2'], int(sample['start2']))
+
+            sentence1_mean = torch.mean(sentence1_vector, dim=0)
+            sentence2_mean = torch.mean(sentence2_vector, dim=0)
+
+            encoded_samples.append(torch.cat((sentence1_mean, sentence2_mean)))
 
             if 'label' in sample:
                 encoded_label = torch.tensor(float(1 if utils.str_to_bool(sample['label']) else 0))
@@ -44,8 +49,24 @@ class WiCDisambiguationDataset(Dataset):
 
         return encoded_samples, encoded_labels
 
+    def __sentence_to_vector(self, sentence: str, target_start: int) -> Tensor:
+        sentence = utils.substitute_spacing_characters(sentence).lower()
+        target_word_index = sentence[:target_start].count(' ')
+
+        sentence_word_vector = list()
+
+        for i, word in enumerate(sentence.split(' ')):
+            if self.stop_words is None or word not in self.stop_words:
+                word = utils.remove_special_characters(word)
+                word_vector = self.word_embeddings[word]
+                if i == target_word_index:
+                    word_vector * self.target_word_weight
+                sentence_word_vector.append(word_vector)
+
+        return torch.stack(sentence_word_vector)
+
     def __len__(self) -> int:
         return len(self.encoded_samples)
 
-    def __getitem__(self, idx) -> Tuple[Tensor, Optional[Tensor]]:
+    def __getitem__(self, idx) -> Tuple[List[Tensor], List[Optional[Tensor]]]:
         return self.encoded_samples[idx], self.encoded_labels[idx]
--- hw1/stud/implementation.py
+++ hw1/stud/implementation.py
@@ -1,6 +1,6 @@
 import stud.utils as utils
 
-from typing import List, Dict
+from typing import List, Dict, Set, Optional
 from model import Model
 from stud.dataset import WiCDisambiguationDataset
 from stud.classifier import WiCDisambiguationClassifier
@@ -8,21 +8,26 @@ from stud.word_embeddings import *
 
 
 def build_model(device: str) -> Model:
-    embedding_size = 200
-    word_embeddings = GloVe(embedding_size=embedding_size)
+    word_embeddings = GloVe(embedding_size=200)
+    weights_path = 'model/weights.pt'
+    n_features = word_embeddings.embedding_size * 2
+    hidden_size = n_features // 2
+    stop_words = utils.load_stop_words('model/stop_words/english')
 
-    return StudentModel(device, word_embeddings)
+    return StudentModel(device, word_embeddings, weights_path, n_features, hidden_size, stop_words)
 
 
 class StudentModel(Model):
 
-    def __init__(self, device: str, word_embeddings: WordEmbeddings, weights_path: str = 'model/weights.pt'):
+    def __init__(self, device: str, word_embeddings: WordEmbeddings, weights_path: str, n_features: int,
+                 hidden_size: int, stop_words: Optional[Set[str]] = None):
         self.word_embeddings = word_embeddings
-        self.model = WiCDisambiguationClassifier(word_embeddings.embedding_size, 100)
+        self.model = WiCDisambiguationClassifier(n_features, hidden_size)
         self.model.load_weights(weights_path, device)
+        self.stop_words = stop_words
 
     def predict(self, sentence_pairs: List[Dict]) -> List[str]:
-        dataset = WiCDisambiguationDataset(sentence_pairs, self.word_embeddings, utils.sample2vector)
+        dataset = WiCDisambiguationDataset(sentence_pairs, self.word_embeddings, self.stop_words)
 
         result = self.model(torch.stack(dataset.encoded_samples))
 
